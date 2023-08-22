6372830117ef503435d3db254b5188517ce030ae
vovkka <vladimirartamonov228@gmail.com>
2023-08-22 19:34:15 +0700

    only cv2

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
-import pygame as pg
+import numpy as np
 import cv2
 
 
 class ArtConverter:
-    def __init__(self, path='image/egor3.jpg', font_size=12, alphabet=' .:!/r(l1Z4H9W8$@'):
-        pg.init()
-        self.path = path
-        self.cv2_image = cv2.imread(self.path)
-        self.image = self.get_image()
-        self.RESOLUTION = self.WIDTH, self.HEIGHT = self.image.shape[0], self.image.shape[1]
-        self.surface = pg.display.set_mode(self.RESOLUTION)
-
+    def __init__(self, font_size=0.2, alphabet=' .:!/r(l1Z4H9W8$@'):
         self.ASCII_CHARS = alphabet
-        self.ASCII_COEFF = self.image.max() // (len(self.ASCII_CHARS) - 1)
-        self.font = pg.font.SysFont('Courier', font_size, bold=True)
-        self.CHAR_STEP = int(font_size * 0.8)
-        self.RENDERED_ASCII_CHARS = [self.font.render(char, False, 'green') for char in self.ASCII_CHARS]
+        self.font_size = font_size
+        self.CHAR_STEP = int(font_size * 40)
+        self.colors = {
+            'green': (0, 255, 0),
+            'blue': (255, 0, 0),
+            'red': (0, 0, 255),
+            'white': (255, 255, 255)
+        }
 
-    def draw_ascii_image(self):
-        char_indices = self.image // self.ASCII_COEFF
-        for x in range(0, self.WIDTH, self.CHAR_STEP):
-            for y in range(0, self.HEIGHT, self.CHAR_STEP):
-                char_index = char_indices[x, y]
-                if char_index:
-                    self.surface.blit(self.RENDERED_ASCII_CHARS[min(char_index, len(self.ASCII_CHARS) - 1)], (x, y))
-
-    def get_image(self):
-        transposed_image = cv2.transpose(self.cv2_image)
-        gray_image = cv2.cvtColor(transposed_image, cv2.COLOR_BGR2GRAY)
+    @staticmethod
+    def get_image(path):
+        image = cv2.imread(path)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return gray_image
 
-    def draw_resized_cv2_image(self):
-        resized_cv2_image = cv2.resize(self.cv2_image, (640, 360), interpolation=cv2.INTER_AREA)
+    def create_ascii_image_cv2(self, path: str, color: str = 'white'):
+        gray_image = self.get_image(path)
+        resolution = width, height = gray_image.shape[0], gray_image.shape[1]
+        ascii_coeff = gray_image.max() // (len(self.ASCII_CHARS) - 1)
+
+        black_image = np.full((*resolution, 3), 0, dtype=np.uint8)
+        char_indices = gray_image // ascii_coeff
+        for x in range(0, width, self.CHAR_STEP):
+            for y in range(0, height, self.CHAR_STEP):
+                char_index = char_indices[x, y]
+
+                cv2.putText(black_image,
+                            self.ASCII_CHARS[min(char_index, len(self.ASCII_CHARS) - 1)],
+                            (y, x),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            self.font_size,
+                            self.colors[color])
+        return black_image
+
+    @staticmethod
+    def draw_resized_cv2_image(cv2_image):
+        resized_cv2_image = cv2.resize(cv2_image, (640, 360), interpolation=cv2.INTER_AREA)
         cv2.imshow('img', resized_cv2_image)
 
-    def draw(self):
-        self.surface.fill('black')
-        self.draw_ascii_image()
+    @staticmethod
+    def save_ascii_image(path, image, name='ascii'):
+        cv2.imwrite(f'{path.strip(".jpg")}_{name}.jpg', image)
 
-    def save_image(self):
-        pygame_image = pg.surfarray.array3d(self.surface)
-        cv2_img = cv2.transpose(pygame_image)
-        cv2.imwrite(f'{self.path.strip(".jpg")}_converted.jpg', cv2_img)
-
-    def run(self):
-        self.draw()
-        self.save_image()
+    def run_photo(self, path: str, color: str = 'white', name='ascii'):
+        ascii_image = self.create_ascii_image_cv2(path, color)
+        self.save_ascii_image(path, ascii_image, name)
 
 
 if __name__ == '__main__':
-    app = ArtConverter('image/vova.jpg', alphabet=' .:!/r(l1Z4H9W8$@')
-    app.run()
+    app = ArtConverter(font_size=0.05)
+    app.run_photo('image/test.jpg', 'green')
