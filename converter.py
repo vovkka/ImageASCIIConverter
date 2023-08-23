a9d311035359c4a4f10875661d0445344b64228a
vovkka <vladimirartamonov228@gmail.com>
2023-08-23 17:53:27 +0700

    annotations

--- converter.py
+++ converter.py
@@ -24,14 +24,14 @@ class ArtConverter:
 
     # Photo processing
     @staticmethod
-    def get_gray_image(path: str = None, image: ndarray = None):
+    def get_gray_image(path: str = None, image: ndarray = None) -> ndarray:
         if path:
             image = cv2.imread(path)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         return gray_image
 
-    def create_ascii_image_cv2(self, gray_image: ndarray, color: str = 'white'):
+    def create_ascii_image_cv2(self, gray_image: ndarray, color: str = 'white') -> ndarray:
         resolution = width, height = gray_image.shape[0], gray_image.shape[1]
         ascii_coeff = gray_image.max() // (len(self.ASCII_CHARS) - 1)
 
@@ -50,18 +50,18 @@ class ArtConverter:
         return black_image
 
     @staticmethod
-    def draw_resized_image(image: ndarray):
+    def draw_resized_image(image: ndarray) -> None:
         resized_image = cv2.resize(image, (640, 360), interpolation=cv2.INTER_AREA)
         cv2.imshow('img', resized_image)
 
-    def save_ascii_image(self, path: str, image: ndarray, name: str = 'ascii'):
+    def save_ascii_image(self, path: str, image: ndarray, name: str = 'ascii') -> None:
         path = f'{path.strip(".jpg")}_{name}.jpg'
         cv2.imwrite(path, image)
         self.log.info(f'Image saved to {path}')
 
     # Video processing
     @staticmethod
-    def get_frames(path: str):
+    def get_frames(path: str) -> tuple[list[ndarray], tuple[int, int], int]:
         video = cv2.VideoCapture(path)
         fps = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
         frame_size = (int(video.get(3)), int(video.get(4)))
@@ -75,7 +75,7 @@ class ArtConverter:
 
         return frames, frame_size, fps
 
-    def convert_to_ascii_frames(self, frames: list[ndarray], color: str = 'white'):
+    def convert_to_ascii_frames(self, frames: list[ndarray], color: str = 'white') -> list[ndarray]:
         self.log.info('Converting to ASCII...')
         ascii_frames = []
         i = 0
@@ -103,12 +103,12 @@ class ArtConverter:
         output.release()
 
     # Main methods
-    def run_video(self, path: str, color: str = 'white', name='ascii'):
+    def run_video(self, path: str, color: str = 'white', name='ascii') -> None:
         frames, frame_size, fps = self.get_frames(path)
         ascii_frames = self.convert_to_ascii_frames(frames, color)
         self.create_ascii_video(path, ascii_frames, frame_size, fps, name)
 
-    def run_photo(self, path: str, color: str = 'white', name='ascii'):
+    def run_photo(self, path: str, color: str = 'white', name='ascii') -> None:
         gray_image = self.get_gray_image(path=path)
         ascii_image = self.create_ascii_image_cv2(gray_image=gray_image, color=color)
         self.save_ascii_image(path, ascii_image, name)
